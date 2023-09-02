4aeba2e450547409c0ca1e825144260a15d4ed85
santhoshkumarcbe <santhoshkumarcbe35@gmail.com>
2023-09-02 17:34:07 +0530

    fibonacci

--- fibonacci.java
+++ fibonacci.java
@@ -1,18 +1,44 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class fibonacci {
     public static void main (String[]args){
-         int n=Integer.valueOf(args[0]);
-          out(n);
+      
+     //  int n=Integer.valueOf(args[0]);
+     //  BigInteger b1=BigInteger.ZERO;
+     //  BigInteger b2=BigInteger.ONE;
+          //  out(b1, b2, n);
+           Scanner in= new Scanner(System.in);
+          byte c=0;
+          if(args[0]=="n"){
+               c++;
+                System.out.println("Program execution stopped thanks for using our service");
+               }
         
+        
+           while (c==0){
+               
+               out(BigInteger.ZERO, BigInteger.ONE, Integer.valueOf(args[0]));
+               args[0]=in.next();
+               if(args[0]=="n"){
+                    c++;
+               System.out.println("Program execution stopped thanks for using our service");
+          }
+           }
+          
+           in.close();
         
     }
+ 
 
-    public static int out(int n1){
-            if(n1<=1){
-                return n1;
-            }
-            
-                return out(n1-1) + out(n1-2);
-            
-
-        }
+    public static int out(BigInteger temp1 , BigInteger temp2, int n1){
+         if(n1!=0){
+                BigInteger temp3=temp1.add(temp2);
+                System.out.print(temp3+" ");
+                temp1=temp2;
+                temp2=temp3;
+                out(temp1,temp2,--n1);     
+         }
+         return 0;  
+    }
 }
